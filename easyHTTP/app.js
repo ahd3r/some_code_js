4cdf2f72c83457b819298df29054291230a325b7
ahd3r <ander11110@gmail.com>
2018-11-30 23:46:39 +0200

    Done httprequest lib

--- easyHTTP/app.js
+++ easyHTTP/app.js
@@ -1,3 +1,43 @@
-const http=new easyHTTP;
+const http = new easyHTTP;
+const firstUser = {
+    "name": "Peyton",
+    "last name": "List",
+    "age": 20,
+};
+const secondUser = {
+    "name": "Ander",
+    "username": "ahd3r",
+    "email": "demo@demo.com",
+};
 
-http.get('https://jsonplaceholder.typicode.com/users/1');
+// http.get('https://jsonplaceholder.typicode.com/users/1', function(err, user){
+//     if(err){
+//         console.log(err);
+//     } else {
+//         console.log(user);
+//     }
+// });
+
+// http.post('https://jsonplaceholder.typicode.com/users/', firstUser, function(err, user){
+//     if(err === null){
+//         console.log(user);
+//     } else {
+//         console.log(err);
+//     }
+// });
+
+// http.put('https://jsonplaceholder.typicode.com/users/4', secondUser, function(err, user){
+//     if(err === null){
+//         console.log(user);
+//     } else {
+//         console.log(err);
+//     }
+// });
+
+// http.delete('https://jsonplaceholder.typicode.com/users/3', function(err, answer){
+//     if(err===null){
+//         console.log(answer);
+//     } else {
+//         console.log(err);
+//     }
+// });
--- easyHTTP/easyhttp.js
+++ easyHTTP/easyhttp.js
@@ -1,14 +1,56 @@
 function easyHTTP(){
-    const http = new XMLHttpRequest;
+    this.http = new XMLHttpRequest;
 }
 
-easyHTTP.prototype.get=function(url){
-    this.http.open('GET', url, true);
+easyHTTP.prototype.get=function(url, callback){
+    this.http.open('GET', url);
+    this.http.onload=()=>{
+        if(this.http.status===200){
+            callback(null, JSON.parse(this.http.responseText));
+        } else {
+            callback(`Error: ${this.http.status}`);
+        }
+    };
+    this.http.send();
+};
 
-    const self = this;
-    this.http.onload=function(){
-        if(self.http.status===200){
-            console.log(self.http.resposeText);
+easyHTTP.prototype.post=function(url, data, callback){
+    this.http.open('POST',url);
+    this.http.setRequestHeader('Content-type', 'application/json');
+    this.http.onload=()=>{
+        if(this.http.status===201){
+            callback(null, this.http.responseText);
+        }else if (this.http.status===404){
+            callback(`Error: ${this.http.status}. Take of a num from the end of url if it there is, because you add it to end in any case`);
+        } else {
+            callback(`Error: ${this.http.status}`);
+        }
+    };
+    this.http.send(JSON.stringify(data));
+};
+
+easyHTTP.prototype.put=function(url, data, callback){
+    this.http.open('PUT', url);
+    this.http.setRequestHeader('Content-type', 'application/json'); // This allow us to use a json data, without it you couldn't use json
+    this.http.onload=()=>{
+        if(this.http.status===200){
+            callback(null, this.http.responseText);
+        }else if(this.http.status===404){
+            callback(`Error: ${this.http.status}. Try to add a num to end of url if it doesn't is their`);
+        }else{
+            callback(`Error: ${this.http.status}`);
+        }
+    };
+    this.http.send(JSON.stringify(data));
+};
+
+easyHTTP.prototype.delete=function(url, callback){
+    this.http.open('DELETE', url);
+    this.http.onload=()=>{
+        if(this.http.status===200){
+            callback(null, 'Deleted');
+        }else{
+            callback(`Error: ${this.http.status}`);
         }
     };
     this.http.send();
