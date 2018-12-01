c5b7c26fe86c98c49957f5e030458693ad47a679
ahd3r <ander11110@gmail.com>
2018-12-01 23:11:45 +0200

    rewrite easyhttp, using async

--- easyHTTP/easyhttp.js
+++ easyHTTP/easyhttp.js
@@ -1,46 +1,34 @@
 class easyHTTP{
-    get(url){
-        return new Promise((resolve, reject)=>{
-            fetch(url)
-            .then((res)=>res.json())
-            .then(data=>resolve(data))
-            .catch(err=>reject(err));   
-        });
+    async get(url){
+        const response = await fetch(url);
+        const resData = await response;
+        return resData;
     }
-    post(url, data){
-        return new Promise((resolve, reject)=>{
-            fetch(url, {
-                method:'POST',
-                headers:{'Content-type':'application/json'},
-                body:JSON.stringify(data),
-            })
-            .then((res)=>res.json())
-            .then(stuff=>resolve(stuff))
-            .catch(err=>reject(err))
+    async post(url, data){
+        const response = await fetch(url, {
+            method:'POST',
+            headers:{'Content-type':'application/json'},
+            body:JSON.stringify(data),
         });
+        const resData = await response.json();
+        return resData;
     }
-    put(url,data){
-        return new Promise((resolve, reject)=>{
-            fetch(url, {
+    async put(url,data){
+        const response = await fetch(url, {
                 method:'PUT',
                 headers:{'Content-type':'application/json'},
                 body:JSON.stringify(data),
-            })
-            .then((res)=>res.json())
-            .then(stuff=>resolve(stuff))
-            .catch(err=>reject(err))
-        });
+            });
+        const resData=await response.json();
+        return resData;
     }
     delete(url){
-        return new Promise((resolve, reject)=>{
-            fetch(url, {
+        const response = await fetch(url, {
                 method:'PUT',
                 headers:{'Content-type':'application/json'},
-            })
-            .then((res)=>res.json())
-            .then(()=>resolve('Deleted'))
-            .catch(err=>reject(err));
-        });
+            });
+        const resData = await 'Deleted';
+        return resData;
     }
 }
 
